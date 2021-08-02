056649393c6364f8d856a9d0b4e782535b8637d7
Eduardo Neves <eduardo.bitencourt007@gmail.com>
2021-08-01 22:37:53 -0400

    session fix

--- install.sh
+++ install.sh
@@ -3,5 +3,8 @@
 sudo apt-get install libnotify-bin
 chmod +x battery-health-notifications
 sudo cp battery-health-notifications /usr/bin
+
+#corrigir esta linha abaixo
 sudo sh -c "echo '* * * * * battery-health-notifications' >> /var/spool/cron/crontabs/"$USER
+
 echo "OK"
